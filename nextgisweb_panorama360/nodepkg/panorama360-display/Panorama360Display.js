c32b66901b76d534ace182941cb4af71cbc75da6
floatline <poltewski@gmail.com>
2023-08-14 19:22:41 +0300

    Add tours to viewer

--- nextgisweb_panorama360/nodepkg/panorama360-display/Panorama360Display.js
+++ nextgisweb_panorama360/nodepkg/panorama360-display/Panorama360Display.js
@@ -23,36 +23,56 @@ import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
 
 
 
-const PhotosphereModal = ({ startPoint, points, panoramaField, setFid, ...props }) => {
-
+const PhotosphereModal = ({ featureId, layerId, panoramaField, ...props }) => {
 
     const getCoords = (point) => {
         const pattern = /-?\d+(\.\d+)?/g;
         const [long, lat] = point.match(pattern).map(x => parseFloat(x));
         return [long, lat]
-    }
+    };
 
-    // nodes are connected in a tree with one startNode and other child nodes
-    const generateNodes = (startPoint, points) => {
+    // nodes are connected in a tree with one startPoint and other child nodes
+    const getNode = async (nodeId) => {
+        const startPoint = await route("feature_layer.feature.item", {
+            id: layerId,
+            fid: nodeId
+        }).get({
+            cache: true,
+            query: {
+                srs: 4326
+            }
+        });
         console.log(startPoint);
-        var nodes = [];
-        var nodeIds = []
-        for (const point of points) {
+        // setStartPoint(sp => feature);
+        const node = await route("feature_layer.feature.collection", {
+            id: layerId
+        }).get({
+            query: {
+                limit: 5,
+                order_by: "distance",
+                distance_geom: startPoint.geom,
+                distance_srs: 4326,
+                srs: 4326,
+                offset: 1,
+                cache: true
+            }
+        }).then(points => {
+            console.log(points)
             const node = {};
-            node['id'] = point.id;
-            node['panorama'] = point.fields[panoramaField];
-            node['gps'] = getCoords(point.geom);
-            node['links'] = [{nodeId: startPoint.id}];
-            nodeIds.push({nodeId: point.id});
-            nodes.push(node);
-        }
-        const startNode = {};
-        startNode['id'] = startPoint.id;
-        startNode['panorama'] = startPoint.fields[panoramaField];
-        startNode['gps'] = getCoords(startPoint.geom);
-        startNode['links'] = nodeIds;
-        nodes.push(startNode);
-        return nodes;
+            node['id'] = startPoint.id;
+            node['panorama'] = startPoint.fields[panoramaField];
+            node['gps'] = getCoords(startPoint.geom);
+            var links = [];
+            for (const point of points) {
+                links.push({
+                    nodeId: point.id,
+                    gps: getCoords(point.geom)
+                })
+            }
+            node['links'] = links;
+            return node;
+        });
+        return node;
     };
 
 
@@ -60,10 +80,8 @@ const PhotosphereModal = ({ startPoint, points, panoramaField, setFid, ...props 
     const photosphereWrapper = useRef(null);
     useEffect(() => {
         console.log("rendering");
-        console.log(startPoint)
         photosphereWrapper.current = new Viewer({
             container: "photosphereContainer",
-            panorama: startPoint.fields[panoramaField],
             size: { height: "inherit", width: "inherit" },
             plugins: [
                 CompassPlugin,
@@ -71,6 +89,9 @@ const PhotosphereModal = ({ startPoint, points, panoramaField, setFid, ...props 
                 [VirtualTourPlugin, {
                     positionMode: "gps",
                     renderMode: "3d",
+                    dataMode: "server",
+                    getNode: getNode,
+                    startNodeId: featureId
                 }]
 
             ]
@@ -78,22 +99,13 @@ const PhotosphereModal = ({ startPoint, points, panoramaField, setFid, ...props 
 
         });
 
-        const virtualTour = photosphereWrapper.current.getPlugin(VirtualTourPlugin);
 
 
-        virtualTour.setNodes(generateNodes(startPoint, points), startPoint.id);
 
-        virtualTour.addEventListener("node-changed", ({node, data}) => {
-            console.log(node, data.fromNode);
-            setFid(node.id);
-            console.log(startPoint)
-        });
-
-
-            // return () => {
-            //     photosphereWrapper.current.destroy()
-            // }
-    }, [startPoint])
+        return () => {
+            photosphereWrapper.current.destroy()
+        }
+    }, [])
 
 
 
@@ -124,55 +136,6 @@ const PhotosphereModal = ({ startPoint, points, panoramaField, setFid, ...props 
 
 export const Panorama360Display = ({ url, featureId, layerId, panoramaField }) => {
 
-    const [startPoint, setStartPoint] = useState(null);
-    const [points, setPoints] = useState(null);
-    const [fid, setFid] = useState(featureId);
-
-
-
-    useEffect(() => {
-        const controller = new AbortController();
-        const getPoints = async () => {
-            console.log("getting points");
-
-            const feature = await route("feature_layer.feature.item", {
-                id: layerId,
-                fid: fid
-            }).get({
-                signal: controller.signal,
-                cache: true,
-                query: {
-                    srs: 4326
-                }
-            });
-            setStartPoint(sp => feature);
-            const nearestPoints = await route("feature_layer.feature.collection", {
-                id: layerId
-            }).get({
-                query: {
-                    limit: 5,
-                    order_by: "distance",
-                    distance_geom: feature.geom,
-                    distance_srs: 4326,
-                    srs: 4326,
-                    offset: 1,
-                    signal: controller.signal,
-                    cache: true
-                }
-            });
-            setPoints(p => nearestPoints);
-            console.log(nearestPoints);
-        }
-
-        getPoints();
-
-
-        return () => {
-            controller.abort()
-        }
-
-    }, [fid])
-
 
     return (<div className="ngw-panorama360-identify-button"
     >
@@ -191,10 +154,9 @@ export const Panorama360Display = ({ url, featureId, layerId, panoramaField }) =
             width="100%"
             onClick={() => {
                 const modal = showModal(PhotosphereModal, {
-                    startPoint,
-                    points,
+                    featureId,
+                    layerId,
                     panoramaField,
-                    setFid,
                     onCancel: e => {
                         modal.destroy();
                     }
